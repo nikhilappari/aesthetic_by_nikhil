1a484e88d5b10ba296a2650e25a0ec0623e930a7
nikhilappari <nikhilappari2006@gmail.com>
2026-09-21 13:07:33 +0530

    Fix Gallery navbar button navigation to /gallery page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,13 +21,6 @@ const Navbar = ({ user, logout }) => {
     }
   };
 
-  const handleGalleryClick = (e) => {
-    if (window.location.pathname === '/') {
-      e.preventDefault();
-      document.getElementById('gallery')?.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
   const handleAboutClick = (e) => {
     if (window.location.pathname === '/') {
       e.preventDefault();
@@ -70,7 +63,7 @@ const Navbar = ({ user, logout }) => {
 
         <ul className={`nav-links ${isMobileMenuOpen ? 'mobile-open' : ''}`}>
           <li><Link to="/" onClick={(e) => { handleHomeClick(e); closeMobileMenu(); }}>Home</Link></li>
-          <li><Link to="/gallery" onClick={(e) => { handleGalleryClick(e); closeMobileMenu(); }}>Gallery</Link></li>
+          <li><Link to="/gallery" onClick={closeMobileMenu}>Gallery</Link></li>
           <li><Link to="/artworks" onClick={closeMobileMenu}>Artworks</Link></li>
           <li><Link to="/#about" onClick={(e) => { handleAboutClick(e); closeMobileMenu(); }}>About</Link></li>
           
